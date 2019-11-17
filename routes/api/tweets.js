6fdadd5a71467b8c0a0eaf20a6591b9145d48114
griffin sharp <gsharpdev@gmail.com>
2019-11-17 00:18:43 -0800

    Add back end routes for tweets and users

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -4,10 +4,11 @@ const passport = require("passport");
 const validateTweetInput = require("../../validation/tweets");
 const Tweet = require("../../models/Tweet");
 
-router.get("/test", (req, res) => {
-    res.json({ msg: "This is the tweets route" });
+router.get("test", (req, res) => {
+    res.json({msg: "This is the tweet route"});
 });
 
+// POST ROUTE (create a new tweet)
 router.post("/",
     passport.authenticate('jwt', { session: false }),
     (req, res) => {
@@ -16,19 +17,45 @@ router.post("/",
         if (!isValid) {
             return res.status(400).json(errors);
         }
-        
+    
         const newTweet = new Tweet({
             user: req.user.id,
             text: req.body.text
         });
 
-
         newTweet
             .save()
             .then(tweet => res.json(tweet));
-
     }
-
 );
+
+// GET ROUTE (for all tweets)
+// no passport authentication because don't matter who the user is for this route, unlike the POST route above. 
+router.get("/", (req, res) => {
+    Tweet
+        // find the tweet based on these parameter (nothing)
+      .find()
+        // newest tweets come first
+      .sort({ date: -1})
+      .then(tweets => res.json(tweets))
+      .catch(err => res.status(400).json(err));
+});
+
+// GET ROUTE (for specific user)
+router.get("/user/:user_id", (req, res) => { 
+    Tweet
+    .find({user: req.params.user_id})
+    .then(tweets => res.json(tweets))
+    .catch(err => res.status.json(err));
+});
+
+// GET ROUTE (for specific tweet)
+router.get("/:id", (req, res) => {
+    Tweet
+    .findById(req.params.id)
+    .then(tweet => res.json(tweet))
+    .catch(err => res.status(400).json(err));
+});
+
 
 module.exports = router;
